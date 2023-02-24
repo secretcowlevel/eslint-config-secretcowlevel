4ca3f0a04424900a534d0c1cc17851d171e9d57a
Justin Reynard <framerate@gmail.com>
2023-02-24 11:32:21 -0800

    New config for react + node (#1)
    * testing new config
    
    * v0.0.3-rc1
    
    * v0.0.3-rc2
    
    * testing new config
    
    * v0.0.3-rc3
    
    * v0.0.3-rc4
    
    * yarn lock

--- index.js
+++ index.js
@@ -1,66 +1,15 @@
 module.exports = {
   env: {
-    node: true,
+    es6: true,
     jest: true,
   },
-  root: true,
-  extends: [
-    "standard-with-typescript",
-    "plugin:jest/recommended",
-    "plugin:jest/style",
-    "plugin:jest-formatting/recommended",
-    "plugin:prettier/recommended",
-    "prettier",
-  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
     sourceType: "module",
+    // type specific stuff
+    tsconfigRootDir: process.cwd(),
+    project: "tsconfig.json",
   },
-  plugins: ["@typescript-eslint", "jest", "jest-formatting", "prettier"],
-  rules: {
-    "@typescript-eslint/strict-boolean-expressions": "warn",
-    "@typescript-eslint/prefer-nullish-coalescing": "warn",
-    "lines-between-class-members": 2,
-    "padding-line-between-statements": "off",
-    "@typescript-eslint/padding-line-between-statements": [
-      "error",
-      {
-        blankLine: "always",
-        prev: "block-like",
-        next: "*",
-      },
-      {
-        blankLine: "always",
-        prev: "*",
-        next: "block-like",
-      },
-      {
-        blankLine: "always",
-        prev: "import",
-        next: "*",
-      },
-      {
-        blankLine: "never",
-        prev: "import",
-        next: "import",
-      },
-      {
-        blankLine: "always",
-        prev: "*",
-        next: ["multiline-expression"],
-      },
-      {
-        blankLine: "always",
-        prev: "*",
-        next: "return",
-      },
-      {
-        blankLine: "always",
-        prev: "*",
-        next: "export",
-      },
-    ],
-    "no-console": "error",
-  },
+
+  extends: ["./rules/base.js", "plugin:prettier/recommended"],
 };
---
+++ rules/base.js
@@ -0,0 +1,52 @@
+module.exports = {
+  extends: [
+    "standard-with-typescript",
+    // "plugin:jest/recommended",
+    // "plugin:jest/style",
+  ],
+  rules: {
+    "@typescript-eslint/strict-boolean-expressions": "warn",
+    "@typescript-eslint/prefer-nullish-coalescing": "warn",
+    "lines-between-class-members": 2,
+    "padding-line-between-statements": "off",
+    "@typescript-eslint/padding-line-between-statements": [
+      "error",
+      {
+        blankLine: "always",
+        prev: "block-like",
+        next: "*",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "block-like",
+      },
+      {
+        blankLine: "always",
+        prev: "import",
+        next: "*",
+      },
+      {
+        blankLine: "never",
+        prev: "import",
+        next: "import",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: ["multiline-expression"],
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "return",
+      },
+      {
+        blankLine: "always",
+        prev: "*",
+        next: "export",
+      },
+    ],
+    "no-console": "error",
+  },
+};
